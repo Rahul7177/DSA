b0697b517efa769a8b74efab039d1dadb037e7cb
Rahul Raj <rahulr7177@gmail.com>
2025-07-14 06:59:32 +0530

    Day 14

---
+++ Linked List/02_MiddleOfLL.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) return head;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    Solution sol;
+    ListNode* mid = sol.middleNode(head);
+
+    while (mid != nullptr) {
+        cout << mid->val << " ";
+        mid = mid->next;
+    }
+    cout << endl;
+    return 0;
+}
